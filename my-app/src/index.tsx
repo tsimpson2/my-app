24df827d401ee6ceec95ac976cf5b7b940bd83ba
Tyler Simpson <tysimpson32@gmail.com>
2022-02-24 15:26:50 -0500

    Completed tutorial for Node

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('h1', {className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
